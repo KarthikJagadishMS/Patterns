4f031df9a69d6d3236dc108a5352552435e3ba66
Karthik <karthik.jagadishms@gmail.com>
2023-04-04 10:01:42 +0530

    Miscellaneous Fixes and Logging

--- src/main/java/com/upgrad/patterns/Service/IndiaDiseaseStatFactory.java
+++ src/main/java/com/upgrad/patterns/Service/IndiaDiseaseStatFactory.java
@@ -4,11 +4,15 @@ import com.upgrad.patterns.Interfaces.IndianDiseaseStat;
 import com.upgrad.patterns.Constants.SourceType;
 import com.upgrad.patterns.Strategies.DiseaseShStrategy;
 import com.upgrad.patterns.Strategies.JohnHopkinsStrategy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class IndiaDiseaseStatFactory {
+
+    private static Logger logger = LoggerFactory.getLogger(IndiaDiseaseStatFactory.class);
     private IndianDiseaseStat diseaseShStrategy;
     private IndianDiseaseStat johnHopkinsStrategy;
 
@@ -21,23 +25,30 @@ public class IndiaDiseaseStatFactory {
         this.johnHopkinsStrategy = johnHopkinsStrategy;
     }
 
- public IndianDiseaseStat GetInstance(SourceType sourceEnum) {
- 
-      //create a method named GetInstance with return type as IndianDiseaseStat and parameter of type sourceType
-    	//create a conditional statement
-    	//if the sourceType is JohnHopkins
-    	//return johnHopkinsStrategy
-    	//if the sourceType is DiseaseSh
-    	//return diseaseShStrategy
-    
-    	//create a message for invalid disease strategy/sourceType
-    	//throw the message as an Illegal argument exception
+
+
+
+
+
+    public IndianDiseaseStat GetInstance(SourceType sourceEnum) {
+
+
+        //create a method named GetInstance with return type as IndianDiseaseStat and parameter of type sourceType
+        //create a conditional statement
+        //if the sourceType is JohnHopkins
+        //return johnHopkinsStrategy
+        //if the sourceType is DiseaseSh
+        //return diseaseShStrategy
+
+        //create a message for
+        //throw the message as an Illegal argument exception
 
         if(sourceEnum.equals(SourceType.JohnHopkins)){
             this.requestedStrategy= johnHopkinsStrategy;
         }else if (sourceEnum.equals(SourceType.DiseaseSh)){
             this.requestedStrategy= diseaseShStrategy;
         }else{
+            logger.error("invalid disease strategy/sourceType");
             throw new IllegalArgumentException("invalid disease strategy/sourceType");
         }
 
